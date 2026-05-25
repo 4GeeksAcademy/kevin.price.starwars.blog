bb72d8229ad44c915a6179d391a15a4f21066ced
Kevin Martín Price <k.pricevisuals@gmail.com>
2026-05-25 17:43:02 +0000

    Archivos arreglados

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
+import {
+    defineConfig
+} from 'vite'
+ plugins: [react()],
+    server: {
+        port: 3000
+    }
+    
+    build: {
+        outDir: 'dist'
+    }
